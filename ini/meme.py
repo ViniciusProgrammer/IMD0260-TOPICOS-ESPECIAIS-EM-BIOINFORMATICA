d4a28acb51c9a39146d7ba944e2361a366713430
ViniciusProgrammer <viniprogrammer@hotmail.com>
2025-09-07 08:58:25 -0300

    update

--- ini/meme.py
+++ ini/meme.py
@@ -1,23 +1,7 @@
-#!/usr/bin/python
-
-'''
-Rosalind: Bioinformatics Armory
-Problem: New Motif Discovery
-URL: http://rosalind.info/problems/meme/
-
-The novel-motif finding tool MEME can be found here:
-http://meme-suite.org/tools/meme
-
-Given: A set of protein strings in FASTA format that share some motif with
-minimum length 20.
-Return: Regular expression for the best-scoring motif.
-'''
 
 
 def main():
     regex = ''
-
-    # Read the plain text file output by the MEME tool.
     with open('rosalind_gbk.txt', 'r') as f:
         for line in f:
             if 'regular expression' in line:
@@ -28,14 +12,9 @@ def main():
                         regex += line.rstrip()
                     else:
                         break
-
-    # Output the answer.
     with open('rosalind_meme.txt', 'w') as outfile:
         outfile.write(regex)
-
-    # Optional: Print the answer.
     print('Regular expression =', regex)
-
 
 if __name__ == '__main__':
     main()
